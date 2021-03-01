7133c891f3c571b86e81167061c4e6b284ade35e
baichuan72f <woshibaichuan@163.com>
2021-03-01 14:16:36 +0800

    V1.0
    DataViewer 版本1.0

---
+++ UITools/Assets/Tools/UI/DataViewer/Scripts/Sender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Sender : MonoBehaviour
+{
+    public string url;
+
+    public int time;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (time == 2)
+        {
+            SendRequest();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendRequest(Action<string> callback = null)
+    {
+        if (!Controller.Instance.dataDic.ContainsKey(url))
+        {
+            Controller.Instance.dataDic.Add(url, "");
+        }
+
+        Controller.Instance.dataDic[url] = "Result:" + Random.value;
+        if (callback != null)
+        {
+            callback("Result:" + Random.value);
+        }
+    }
+}
---
+++ UITools/Assets/Tools/UI/DataViewer/Scripts/Shower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shower : MonoBehaviour
+{
+    public string url;
+    public string path;
+
+    public int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time == 2 && Time.frameCount % 100 == 0)
+        {
+            if (Controller.Instance.dataDic.ContainsKey(url))
+            {
+                GetComponent<Text>().text = Controller.Instance.dataDic[url];
+            }
+        }
+    }
+}
