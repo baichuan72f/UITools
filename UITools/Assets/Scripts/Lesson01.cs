c1527d73eb1f558de2bdc8157efd0505ea9ed263
baichuan72f <woshibaichuan@163.com>
2021-04-06 17:53:22 +0800

    Add UniRx And Ad

---
+++ UITools/Assets/Scripts/Lesson01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Lesson01 : MonoBehaviour
+{
+    private DataProxy m_DataProxy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_DataProxy = new DataProxy("15888888888", "123456");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            
+        }
+    }
+}
